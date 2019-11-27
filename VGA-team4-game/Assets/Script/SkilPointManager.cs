6ef3acff334946e953c7aa845090644a71f456ae
hujishima takumi <hujishima.t425@gmail.com>
2019-11-28 08:18:39 +0900

    change script

--- VGA-team4-game/Assets/Script/SkilPointManager.cs
+++ VGA-team4-game/Assets/Script/SkilPointManager.cs
@@ -9,6 +9,7 @@ public class SkilPointManager : MonoBehaviour
     [SerializeField] public float m_atackMaguni = 1.1f;
     [SerializeField] public float m_speedMaguni = 1.1f;
     [SerializeField] public int m_skilPoint = 0;
+    [SerializeField] public float m_helthRate = 1f;
     //GameObject m_playerData;
     //Component m_player;
     // Start is called before the first frame update
@@ -23,8 +24,16 @@ public class SkilPointManager : MonoBehaviour
         
     }
 
-    public void setSkilPoint(int skilpoint)
+    public void SetSkilPoint(int skilpoint)
     {
         m_skilPoint += skilpoint;
+    }
+
+    public void AddHelthEX()
+    {
+        if (m_skilPoint < 0)
+        {
+            m_helthEX = (int)Mathf.Ceil(m_helthEX * m_helthRate);
+        }
     }
 }
