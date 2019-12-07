2a2f28e44caa2bd7f42430f1caa4758064f95c7c
hujishima takumi <hujishima.t425@gmail.com>
2019-12-07 11:23:41 +0900

    create EnemyGeneleter

---
+++ VGA-team4-game/Assets/Script/EnemyGeneleter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGeneleter : MonoBehaviour
+{
+    [SerializeField] GameObject m_enemyObj;
+    [SerializeField] float m_taimeMargin = 1f;
+    float m_taimer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_taimer = Time.deltaTime;
+        if (m_enemyObj)
+        {
+            if (m_taimer >= m_taimeMargin)
+            {
+                Instantiate(m_enemyObj, this.gameObject.transform.position, Quaternion.identity);
+                m_taimer = 0;
+            }
+        }
+    }
+}
