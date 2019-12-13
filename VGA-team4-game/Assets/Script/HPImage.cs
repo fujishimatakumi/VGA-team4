7e6353e346edd0553c28dfe7eea0b2fcb6e007d4
hujishima takumi <hujishima.t425@gmail.com>
2019-12-14 02:15:33 +0900

    resolve eroor

--- VGA-team4-game/Assets/Script/HPImage.cs
+++ VGA-team4-game/Assets/Script/HPImage.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HPImage : MonoBehaviour
 {
@@ -14,7 +15,7 @@ public class HPImage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        m_im gaugeCtrl = GetComponent<Image>();
+        Image gaugeCtrl = GetComponent<Image>();
         gaugeCtrl.fillAmount =0 ;
     }
 }
