ab41291af801c7b86edb12a0e0cb943649d7fd97
hujishima takumi <hujishima.t425@gmail.com>
2019-12-20 02:29:12 +0900

    save

--- VGA-team4-game/Assets/Script/CameraControlle.cs
+++ VGA-team4-game/Assets/Script/CameraControlle.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class CameraControlle : MonoBehaviour
 {
-    
+    Camera m_mc;
+    Vector3 m_anker;
+    Transform m_target;
     // Start is called before the first frame update
     void Start()
     {
-       
+        m_mc = this.gameObject.transform.GetComponentInChildren<Camera>();
+        m_anker = m_mc.gameObject.transform.position;
+        m_target = this.gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horiR = Input.GetAxis("HorizontalR");
-        float verR = Input.GetAxis("VerticalR");
+        
+        float horiR = Input.GetAxisRaw("HorizontalR");
+        float verR = Input.GetAxisRaw("VerticalR");
         Vector3 dir = Vector3.up * horiR + Vector3.right * verR;
+        
 
-        if ((horiR != 0) || (verR != 0))
+        if (dir == Vector3.zero)
         {
-            this.gameObject.transform.Rotate(dir);
+            this.gameObject.transform.Rotate(Vector3.zero);
         }
         else
         {
-            this.gameObject.transform.Rotate(Vector3.zero);
+            this.gameObject.transform.Rotate(dir);
         }
     }
 }
