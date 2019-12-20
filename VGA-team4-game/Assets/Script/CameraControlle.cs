fe9b5f7dad6819a04a9f221b72a6f101fbf5575b
hujishima takumi <hujishima.t425@gmail.com>
2019-12-20 13:18:41 +0900

    change script

--- VGA-team4-game/Assets/Script/CameraControlle.cs
+++ VGA-team4-game/Assets/Script/CameraControlle.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class CameraControlle : MonoBehaviour
 {
-    Camera m_mc;
-    Vector3 m_anker;
-    Transform m_target;
+   
+    
     // Start is called before the first frame update
     void Start()
     {
-        m_mc = this.gameObject.transform.GetComponentInChildren<Camera>();
-        m_anker = m_mc.gameObject.transform.position;
-        m_target = this.gameObject.transform;
+       
     }
 
     // Update is called once per frame
