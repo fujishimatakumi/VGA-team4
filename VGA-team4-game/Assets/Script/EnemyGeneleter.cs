92fcba90bbcc0908aa41f1f9e9f5827b57286f2d
hujishima takumi <hujishima.t425@gmail.com>
2020-01-07 13:46:12 +0900

    change script

--- VGA-team4-game/Assets/Script/EnemyGeneleter.cs
+++ VGA-team4-game/Assets/Script/EnemyGeneleter.cs
@@ -8,11 +8,14 @@ public class EnemyGeneleter : MonoBehaviour
     [SerializeField] float m_taimeMargin = 1f;
     float m_taimer = 0;
     [SerializeField] bool m_flag = false;
+    public static int m_rimit;
+    GameStatusManager m_gameStatus;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        GameObject go = GameObject.Find("GameStatusManager");
+        m_gameStatus = go.GetComponent<GameStatusManager>();
     }
 
     // Update is called once per frame
@@ -26,9 +29,18 @@ public class EnemyGeneleter : MonoBehaviour
                 if (m_taimer >= m_taimeMargin)
                 {
                     Instantiate(m_enemyObj, this.gameObject.transform.position, Quaternion.identity);
+                    m_rimit++;
                     m_taimer = 0;
                 }
             }
+        }
+        if (m_rimit >= m_gameStatus.m_clearRimit)
+        {
+            StopGenerat();
+        }
+        else if (m_rimit < m_gameStatus.m_clearRimit)
+        {
+            StartGenerat();
         }
     }
 
