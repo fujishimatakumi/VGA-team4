7ef7dec89a6f76f5de78830c3d7f57cb78587b5a
nittayoshihiro <yosshi3800@gmail.com>
2020-01-23 17:50:04 +0900

    map

---
+++ VGA-team4-game/Assets/Scenes/Script/PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float m_moveSpead = 1f;
+    [SerializeField] int m_helth = 100;
+    [SerializeField] int m_level = 1;
+    [SerializeField] float m_Atack = 1f;
+    [SerializeField] float m_resaltMargin = 3f;
+    Rigidbody m_rb;
+    Animator m_anim;
+    //[SerializeField] Slider m_HPslider = default;
+    [SerializeField] GameObject m_hitCollider;
+    [SerializeField] Image m_im = default;
+    
+
+    //GameObject m_skilPoint;
+   // SkilPointManager m_manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+        m_anim = GetComponent<Animator>();
+        m_im.fillAmount = m_helth;
+        //m_HPslider.value = m_helth;
+       // m_skilPoint = GameObject.Find("SkilPointManager");
+     //   m_manager = m_skilPoint.GetComponent<SkilPointManager>();
+       // m_helth = m_helth + m_manager.m_helthEX;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+        Vector3 dir = Vector3.forward * v + Vector3.right * h;
+        if (dir == Vector3.zero)
+        {
+            m_rb.velocity = new Vector3(0f,m_rb.velocity.y,0f);
+            m_anim.SetBool("Moving", false);
+        }
+        else
+        {
+            m_anim.SetBool("Moving", true);
+            dir = Camera.main.transform.TransformDirection(dir);
+            dir.y = 0;
+            this.transform.forward = dir;
+            
+            Vector3 velo = this.transform.forward * m_moveSpead;
+            velo.y = m_rb.velocity.y;
+            m_rb.velocity = velo;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            m_anim.SetTrigger("Attack1Trigger");
+        }
+        if (Input.GetKey("p"))
+        {
+            m_helth = 0;
+        }
+            
+
+        
+
+        if (m_helth <= 0)
+        {
+            
+                GameStatusManager gm = FindObjectOfType<GameStatusManager>();
+                gm.GameOver();
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            GameObject ga = other.gameObject;
+            EnemyContoroller ec = ga.GetComponent<EnemyContoroller>();
+            ec.DecreaseHelth();
+            ec.UpDateSrider();
+        }
+    }
+
+    public void ActivCollider()
+    {
+        m_hitCollider.SetActive(true);
+    }
+
+    public void EnableCollider()
+    {
+        m_hitCollider.SetActive(false);
+    }
+
+    public void HitDamage(int damege)
+    {
+        m_im.fillAmount -= damege;
+    }
+}
---
+++ VGA-team4-game/Assets/Scenes/Script/SceneLode.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLode : MonoBehaviour
+{
+    public static bool m_resultFlug = true;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_resultFlug)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                LodeSceneMain();
+            }
+        }
+        else if (!m_resultFlug)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                LodeSceneOpen();
+            }
+        }
+    }
+
+    public void LodeSceneMain()
+    {
+        Initiate.Fade("mainScene",Color.black,2f);
+        m_resultFlug = false;
+    }
+
+    public void LodeSceneOpen()
+    {
+        Initiate.Fade("openScene",Color.black,2f);
+        m_resultFlug = true;
+    }
+    
+}
