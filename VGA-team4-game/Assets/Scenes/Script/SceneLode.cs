308568099a905008bd72aa100497a5d1f59bc499
hujishima takumi <hujishima.t425@gmail.com>
2020-01-24 23:17:36 +0900

    create jump

--- VGA-team4-game/Assets/Scenes/Script/SceneLode.cs
+++ VGA-team4-game/Assets/Scenes/Script/SceneLode.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneLode : MonoBehaviour
 {
+    public bool m_resultFlug = true;
 
     // Start is called before the first frame update
     void Start()
