3ae895eb91b92ba778d20645f855bec6228093be
hujishima takumi <hujishima.t425@gmail.com>
2020-01-30 22:33:16 +0900

    change script

--- VGA-team4-game/Assets/Scenes/Script/EnemyGeneleter.cs
+++ VGA-team4-game/Assets/Scenes/Script/EnemyGeneleter.cs
@@ -16,6 +16,7 @@ public class EnemyGeneleter : MonoBehaviour
     {
         GameObject go = GameObject.Find("GameStatusManager");
         m_gameStatus = go.GetComponent<GameStatusManager>();
+        m_rimit = 0;
         StartGenerat();
     }
 
